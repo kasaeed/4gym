d9448d5f57fd276b93cbc55bf121299c7c5717a2
kasaeed <138814143+kasaeed@users.noreply.github.com>
2023-07-08 11:14:47 +0330

    Update __init__.py

--- gym_stock/__init__.py
+++ gym_stock/__init__.py
@@ -1,7 +1,9 @@
 from gym.envs.registration import register
+import sys
+sys.modules[__name__]
 
 register(
     id='hedge-v0',
-    entry_point='gym_stock.envs:DeltaHedge',
+    entry_point='__main__:DeltaHedge',
     timestep_limit=1000,
 )
