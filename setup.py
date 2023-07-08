3a6433e7d064f9e71acb2f7673e5ab74a24168c3
kasaeed <138814143+kasaeed@users.noreply.github.com>
2023-07-08 11:02:03 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name="gym_stock",
       version="0.0.1",
-      url="https://github.com/...",
+      url="https://github.com/kasaeed/gym-stock",
       author="Saeed Karimi",
-      packages=["gym_stock", "gym_stock.envs"],
+      # packages=["gym_stock", "gym_stock.envs"],
       package_data = {},
-      install_requires = ["gym", "numpy"]
+      install_requires = ["gym", "numpy", "scipy"]
 )
