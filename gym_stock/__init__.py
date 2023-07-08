dfc52ac80ec0b8222445824e057ebae24fc3e773
kasaeed <138814143+kasaeed@users.noreply.github.com>
2023-07-08 10:00:52 +0330

    Update __init__.py

--- gym_stock/__init__.py
+++ gym_stock/__init__.py
@@ -1,1 +1,7 @@
-from gym_stock.envs.stock_env import *
+from gym.envs.registration import register
+
+register(
+    id='hedgin-v0',
+    entry_point='gym_stock.envs:DeltaHedge',
+    timestep_limit=1000,
+)
